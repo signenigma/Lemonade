69d93c66077a008ff440b72ad41860040ba1e11e
signenigma <sign.e.nigma@gmail.com>
2014-08-12 10:34:12 +0200

    første commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <!-- Viewport skal sættes for at sitet bliver responsive -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        
+        <!-- Her linkes til Lemonade Grid'ets CSS. Den skal der IKKE redigeres i medmindre du absolut kun vil rette i GRID'et! -->
+        <link rel="stylesheet" type="text/css" href="lemonade.css">
+        
+        <!-- Her linkes til sitets eget CSS. Her rettes der i sitets udseende -->
+        <link rel="stylesheet" type="text/css" href="kittehs.css">
+        <title>Lemonade</title>
+    </head>
+    
+    <body>
+        <div class="frame">
+
+            <header class="header col-1">
+                <h1>When life gives you Lemons</h1>
+                <p>Google some cute Kittehs </p>
+            </header>
+
+            <div class="section-upper">
+
+                <div class="col-3">
+                    <img src="http://placekitten.com/g/600/300" />
+
+                </div>
+                <div class="col-3">
+                    <img src="http://placekitten.com/g/600/300" />
+
+                </div>
+                <div class="col-3">
+                    <img src="http://placekitten.com/g/600/300" />
+
+                </div>
+            </div>
+
+            <div class="section-lower">
+
+                <div class="col-1-3">
+                    <h4>Here is a 1/3 column! Yay!</h4>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>  </div>
+
+                <div class="col-2-3">
+                    <h4>Here is the rest: a 2/3 column! Yay!</h4>
+                    <p>More kittehs! Yay!</p>
+                </div>
+                
+                <div class="col-1"></div>
+
+                <div class="col-3">
+                    <h3>Placekitten</h3>
+                    <img src="http://placekitten.com/800/400"/>
+                </div>
+                <div class="col-3">
+                    <h3>Placekitten</h3>
+                    <img src="http://placekitten.com/800/400"/>
+                </div>
+                <div class="col-3">
+                    <h3>Placekitten</h3>
+                    <img src="http://placekitten.com/800/400"/>
+                </div>
+                                
+
+            </div>
+        </div>
+    </body>
+</html>
